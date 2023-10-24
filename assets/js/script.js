a4fa892c89fb5b74bf106afa87f1ac6e602f21e1
Souza <114781129+Souzinhaaz@users.noreply.github.com>
2023-10-24 09:29:08 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function mostrarSenha(){
+    var inputPass = document.getElementById('password')
+    var btnShowPass = document.getElementById('btn-senha')
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type', 'text')
+        btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+    }else{
+        inputPass.setAttribute('type', 'password')
+    btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
+}
+}
+
